1c929bc6d25e457e68673dc8a62feec7eb8f813f
Zack Baker <zack.baker@protonmail.ch>
2020-02-10 14:03:07 -0700

    Updated runner file

--- src/smoothclimb.cpp
+++ src/smoothclimb.cpp
@@ -3,14 +3,19 @@
 
 int main(int argc, char const *argv[])
 {
-	int size = 14;
-	Board b = Board(size, 0.1);
+	int size = 10;
+	Board b = Board(size, 6, 0.05);
 	std::cout << b << std::endl;
 	std::cout << b.getTotalSpaces() << std::endl;
-	std::vector<std::tuple<int,int>> tuples = b.getLadders();
+	std::vector<int*> tuples = b.getLadders();
 	std::cout << "Ladders: \n" << std::endl;
-	for(std::tuple<int,int> tup: tuples){
-		std::cout << "(" << std::get<0>(tup) << "," << std::get<1>(tup) << ")" << std::endl;
+	for(int* pair: tuples){
+		std::cout << "(" << pair[0] << "," << pair[1] << ")" << std::endl;
+	}
+
+	std::vector<int> rolls = b.calculateOptimalRolls();
+	for(int i=1;i<rolls.size();i++){
+		std::cout << "Roll " << i << " is " << rolls.at(i-1) << std::endl;
 	}
 
 }
