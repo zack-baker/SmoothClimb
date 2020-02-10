cdb38f73b0cb405efe9f74a63c23a330553215a7
Zack Baker <zack.baker@protonmail.ch>
2020-02-10 14:02:44 -0700

    Added myopic optimal roll algorithm

--- src/board.cpp
+++ src/board.cpp
@@ -3,13 +3,15 @@
 /**
 	Constructor for the board object
 	@param s - the size of the board. Since the board is a 2d square, we only need one side length
+	@param ds - the size of the dice
 	@param ladderChance - a double between 0 (inc.) and 1(exc.) used to determine if any particular space
 	is a ladder source
 
 */
-Board::Board(int s, double ladderChance){
-	/** Assign the size */
+Board::Board(int s, int ds, double ladderChance){
+	/** Assign the private members */
 	size = s;
+	dicesize = ds;
 
 	/** initialize ladders */
 	//initialize the random number geneartion objects
@@ -37,20 +39,19 @@ Board::Board(int s, double ladderChance){
 			}
 			//if sourceAvail is false, then there are no possible sinks on the rest of the board, so stop attempting to generate ladders
 			if(!sinkAvail){
-				std::cout << "[DEBUG] No available sinks after space " << space << std::endl;
-				for(int i=0; i< getTotalSpaces();i++){
-					std::cout << "[DEBUG] sink available at space " << i << ": " << takenSinks[i] << std::endl;
-				}
 				break;
 			}
 			//create a distribution object 
-			std::uniform_int_distribution<int> sinkDist(firstNextRowSpace, getTotalSpaces()+1);
+			std::uniform_int_distribution<int> sinkDist(firstNextRowSpace, getTotalSpaces());
 			int sink;
 			do{
 				sink = sinkDist(generator);
 			}while(takenSinks[sink]);
 			takenSinks[sink] = true;
-			ladders.push_back(std::make_tuple(space, sink));
+			int* ladderArr = new int[2];
+			ladderArr[0] = space;
+			ladderArr[1] = sink;
+			ladders.push_back(ladderArr);
 		}
 	}
 
@@ -58,13 +59,17 @@ Board::Board(int s, double ladderChance){
 	delete [] takenSinks;
 }
 
-Board::~Board(){}
+Board::~Board(){
+	for(int i=0;i<ladders.size();i++){
+		delete [] ladders.at(i);
+	}
+}
 
 /** Getter function definitions */
 int Board::getSize(){
 	return size;
 }
-std::vector<std::tuple<int,int>> Board::getLadders(){
+std::vector<int*> Board::getLadders(){
 	return ladders;
 }
 
@@ -75,6 +80,63 @@ void Board::setSize(int s){
 /** dynamically calcuate the total number of spaces on the board */
 int Board::getTotalSpaces(){
 	return size*size;
+}
+
+/** 
+*	This method finds the optimal set of rolls to win the current board.
+*	It is important to note that this algorithm is myopic, meaning it will only
+*	look ahead to the next roll to determine which move will move it the furthest. 
+*	To illustrate this, imagine the following scenario (dice size = 6):
+*
+*	The player has just started the game and is on space 0. Two ladders exist on the board:
+		- One on space 5, which takes the player to space 40
+		- One on space 7, which takes the player to space 90
+
+*	The algorithm looks at spaces 1-6 for ladders, and finds the ladder on space 5. Since this 
+*	ladder takes the player past space 6, the maximum "normal" roll, this is deemed the optimal roll,
+*	and moves the piece here. On subsequent turns, the player moves 6 spaces until they reach space 100 on turn 11.
+*	The actual optimal set of rolls for this board would be to move 6 on turn 1, bypassing the first ladder, and moving 1 on
+*	the next turn, landing on the ladder on space 7, travelling all the way to space 90 on turn 2, and winning on turn 4. 
+*/	
+std::vector<int> Board::calculateOptimalRolls(){
+	//start on the first space
+	int curSpace = 0;
+	std::vector<int> rolls;
+	while(curSpace!=getTotalSpaces()){
+		std::cout << "Starting loop; current space is " << curSpace << std::endl;
+		//track the next space to end the turn, including after potentially following a ladder. Defaults to the space that is dicesize spaces away, i.e. the highest number of spaces one can move on a 'regular' roll of the die
+		int destinationSpace = curSpace + dicesize;
+		std::cout << "Default destinationSpace is " << destinationSpace << std::endl;
+		//similarily, this variable tracks the number the player needs to roll to move to the next ideal space. defaults to the max number the player can roll, unless the player is near the end of the board
+		int roll = dicesize;
+		//if the end space is within the space of a normal roll, don't bother checking ladders, just roll and move to the end with the exact value.
+		if(getTotalSpaces()-curSpace<=dicesize){
+			std::cout << "End space in sight, moving to end space" << std::endl;
+			roll = getTotalSpaces()-curSpace;
+			rolls.push_back(roll);
+			curSpace += roll;
+			break;
+		}
+		std::cout << "Searching ladders" << std::endl;
+		for(int i=0;i<ladders.size();i++){
+			//if a ladder space is less than the maximum number of spaces away this turn, 
+			int* ladder = ladders.at(i);
+			int distance = ladder[0] - curSpace;
+			std::cout << "[Ladder] distance to ladder at " << ladder[0] << ": " << distance << std::endl;
+			//if the ladder is within move-range and the ladder destination is the furthest space seen so far, set this roll as the space to move.
+			if((distance < dicesize && distance > 0) && (ladder[1]>destinationSpace)){
+				std::cout << "[Ladder] Valid ladder found at space " << ladder[0];
+				roll = distance;
+				destinationSpace = ladder[1];
+				std::cout << " Roll to move to this ladder is " << roll << " and ends on space " << destinationSpace << std::endl;
+			}
+		}
+		std::cout << "Ideal roll is " << roll << ", next starting space is " << destinationSpace << std::endl;
+		rolls.push_back(roll);
+		curSpace = destinationSpace;
+	}
+	return rolls;
+
 }
 
 /** Overload the << operator to easily print the board */
